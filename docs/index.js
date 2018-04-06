9f0b9ec72a3f24932a7310b59d1c088767de2fcc
ikasty <mail.ikasty@gmail.com>
2018-04-07 02:35:06 +0900

    fix percent length

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,6 @@
 const opt = {
     color: '#FCB03C',
+    trailColor: '#AAA',
     strokeWidth: 20,
     trailWidth: 20,
     duration: 1000,
@@ -15,7 +16,7 @@ function value(type) {
 };
 
 function percent(value) {
-    return (value * 100).toFixed(8).toString().substring(0, 9);
+    return (value * 100).toFixed(6);
 }
 
 window.onload = function () {
@@ -26,23 +27,23 @@ window.onload = function () {
     const hour  = new ProgressBar.Circle('#hour', opt);
 
     function draw() {
-        const year_value  = value('year');
-        const month_value = value('month');
-        const week_value  = value('week');
-        const day_value   = value('day');
-        const hour_value  = value('hour');
+        const year_p  = value('year');
+        const month_p = value('month');
+        const week_p  = value('week');
+        const day_p   = value('day');
+        const hour_p  = value('hour');
 
-        year   .animate(year_value);
-        month  .animate(month_value);
-        week   .animate(week_value);
-        day    .animate(day_value);
-        hour   .animate(hour_value);
+        year  .animate(year_p);
+        month .animate(month_p);
+        week  .animate(week_p);
+        day   .animate(day_p);
+        hour  .animate(hour_p);
 
-        year   .setText('<p class="caption">year: '  + moment().format('Y')  + '</p>' + percent(year_value));
-        month  .setText('<p class="caption">month: ' + moment().format('MM') + '</p>' + percent(month_value));
-        week   .setText('<p class="caption">week: '  + moment().format('WW') + '</p>' + percent(week_value));
-        day    .setText('<p class="caption">day: '   + moment().format('DD') + '</p>' + percent(day_value));
-        hour   .setText('<p class="caption">hour: '  + moment().format('HH') + '</p>' + percent(hour_value));
+        year  .setText(`<p class="caption">year:  ${moment().format('Y' )}</p><p class="percent">${percent(year_p)}</p>`);
+        month .setText(`<p class="caption">month: ${moment().format('MM')}</p><p class="percent">${percent(month_p)}</p>`);
+        week  .setText(`<p class="caption">week:  ${moment().format('WW')}</p><p class="percent">${percent(week_p)}</p>`);
+        day   .setText(`<p class="caption">day:   ${moment().format('DD')}</p><p class="percent">${percent(day_p)}</p>`);
+        hour  .setText(`<p class="caption">hour:  ${moment().format('HH')}</p><p class="percent">${percent(hour_p)}</p>`);
     };
 
     setInterval(draw, 1000);
